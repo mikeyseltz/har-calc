63830fc20fea0ec22ab030e5185f91e9c3cda2e0
Mike Seltzer <mikeyseltz@gmail.com>
2020-07-26 15:23:10 -0700

    dives added

--- har.py
+++ har.py
@@ -35,15 +35,6 @@ def g_performance(vel, g):
 	return (rate, radius)
 
 # phase 1: first 90
-def first_90(vel, g, level=True): # returns (time, alt delta, range delta)
-	if level:
-		time = (90 / g_performance(vel, g)[0]) + (g/g_onset) # over simplified, need to integrate over time if want better acc.
-		alt = 0
-		rng = -g_performance(vel, g)[1] # no penalty for g onset...need calculus
-		return {'time': time, 'alt': alt, 'rng': rng}
-	else:
-		pass
-
 
 # phase 2:  second 90
 
@@ -54,8 +45,41 @@ def first_90(vel, g, level=True): # returns (time, alt delta, range delta)
 # phase 5:  recover to level
 
 
+def first_90(vel, g, dive=0): # returns (time, alt delta, range delta)
+	time = (90 / g_performance(vel, g)[0]) + (g/g_onset) # over simplified, need to integrate over time if want better acc.
+	alt = 0
+	rng = -g_performance(vel, g)[1] # no penalty for g onset...need calculus // range is negative, ie. closure
+	return {'time': time, 'alt': alt, 'rng': rng}
+	
+def second_90(vel, g, dive): # returns (time, alt delta, range delta)
+	time = 90 / g_performance(vel, g)[0]
+	alt = sin(radians(dive))*g_performance(vel, g)[1]
+	rng = cos(radians(dive))*g_performance(vel, g)[1] # range is increasing now
+	return {'time': time, 'alt': alt, 'rng': rng}
 
+def accel_in_dive(start_vel, end_vel, dive): # should we be using mach or TAS?
+	accel_rate = 50 # const accel rate of 50 knots per second
+	time = (end_vel - start_vel) / accel_rate
+	sin_dive = sin(radians(dive))
+	cos_dive = cos(radians(dive))
+	# get vertical descent rates in feet per second
+	st_vert_rate = sin_dive*start_vel*1.68781 # converts knots to fps
+	end_vert_rate = sin_dive*end_vel*1.68781
+	# get horizontal travel rates in fps
+	st_horiz_rate = cos_dive*start_vel*1.68781
+	end_horiz_rate = cos_dive*end_vel*1.68781
+	# calculate altitude lost during acceleration
+	alt = (st_vert_rate * time) + ((end_vert_rate * time)-(st_vert_rate*time))/2
+	# calculate range delta during accel
+	rng = (st_horiz_rate * time) + ((end_horiz_rate * time)-(st_horiz_rate*time))/2
+	return {'time': time, 'alt': alt, 'rng': rng}
 
+def straight_dive(vel, dive, st_alt): # terminates at 4500' <-- hard coded for now
+	descent_rate = sin(radians(dive))*vel*1.68781
+	alt_to_lose = st_alt - 4500
+	time = alt_to_lose / descent_rate # time in seconds until at 4500'
+	rng = cos(radians(dive))*vel*1.68781*time
+	return {'time': time, 'alt': alt_to_lose, 'rng': rng}
 
 
 
