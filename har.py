76d6e089cb729c9c0f9c560bba15b571f44bd9ba
Mike Seltzer <mikeyseltz@gmail.com>
2020-07-27 17:05:10 -0700

    code sytling

--- har.py
+++ har.py
@@ -5,58 +5,76 @@ from utils import Calculator
 
 def main():
 
-	st_alt = float(input("start altitude in feet >>> "))
-	st_vel = float(input("start speed in knots >>> "))
-	st_rng = float(input("starting range to tgt in nm >>> "))*6036
-	delay = float(input("how long to react in seconds >>> "))
-	tgt_dive = float(input("target dive angle >>> "))
-	g = float(input("how many gs will you pull? >>> "))
-	end_vel = float(input('what will you accelerate to in ktas? >>> '))
+    st_alt = float(input("start altitude in feet >>> "))
+    st_vel = float(input("start speed in knots >>> "))
+    st_rng = float(input("starting range to tgt in nm >>> "))*6036
+    delay = float(input("how long to react in seconds >>> "))
+    tgt_dive = float(input("target dive angle >>> "))
+    g = float(input("how many gs will you pull? >>> "))
+    end_vel = float(input('what will you accelerate to in ktas? >>> '))
 
-	e = Engagement({'alt': st_alt, 'vel': st_vel, 'end_vel' : end_vel, 'rng': st_rng, 'dive': 0, 'g' : g, 'alive': True, 'time': 0, 'target_dive' : tgt_dive, 'delay' : delay})
-	e.execute()
+    e = Engagement({'alt': st_alt,
+                    'vel': st_vel,
+                    'end_vel': end_vel,
+                    'rng': st_rng,
+                    'dive': 0, 'g': g,
+                    'alive': True,
+                    'time': 0,
+                    'target_dive': tgt_dive,
+                    'delay': delay})
+
+    e.execute()
+
 
 class Engagement:
 
-	def __init__(self, state, missile=None):
-		self.missile = missile
-		self.state = state
+    def __init__(self, state, missile=None):
+        self.missile = missile
+        self.state = state
 
-	def execute(self):
-		c = Calculator(3) # 3 second g-onset hard coded for now
-		dive = self.state['target_dive']
-		print(f"starting at {self.state['alt']}ft and {self.state['vel']} knots, diving to {dive} degrees")
+    def execute(self):
+        c = Calculator(3)   # 3 second g-onset hard coded for now
+        dive = self.state['target_dive']
+        print(f"starting at {self.state['alt']}ft and"
+              " {self.state['vel']} knots, diving to {dive} degrees")
 
-		self.update_state(c.time_to_act(self.state['delay'], self.state['vel']))
-		self.update_state(c.first_90(self.state['vel'], self.state['g']))
-		self.update_state(c.second_90(self.state['vel'], self.state['g'], dive))
-		self.update_state(c.accel_in_dive(self.state['vel'], self.state['end_vel'], self.state['dive']))
-		self.update_state(c.straight_dive(self.state['vel'], self.state['dive'],self.state['alt']))
-		self.update_state(c.recover_to_level(self.state['vel'], self.state['dive'], self.state['alt']))
+        self.update_state(c.time_to_act(self.state['delay'],
+                                        self.state['vel']))
+        self.update_state(c.first_90(self.state['vel'],
+                                     self.state['g']))
+        self.update_state(c.second_90(self.state['vel'],
+                                      self.state['g'], dive))
+        self.update_state(c.accel_in_dive(self.state['vel'],
+                                          self.state['end_vel'],
+                                          self.state['dive']))
+        self.update_state(c.straight_dive(self.state['vel'],
+                                          self.state['dive'],
+                                          self.state['alt']))
+        self.update_state(c.recover_to_level(self.state['vel'],
+                                             self.state['dive'],
+                                             self.state['alt']))
 
-	def update_state(self, deltas):
-		starting = self.state
-		self.state['alt'] = starting['alt'] - deltas['alt']
-		self.state['rng'] = starting['rng'] + deltas['rng']
-		self.state['time'] = starting['time'] + deltas['time']
-		try:
-			self.state['dive'] = deltas['dive'] 
-		except:
-			pass
-		try:
-			self.state['vel'] = deltas['vel']
-		except:
-			pass
+    def update_state(self, deltas):
+        starting = self.state
+        self.state['alt'] = starting['alt'] - deltas['alt']
+        self.state['rng'] = starting['rng'] + deltas['rng']
+        self.state['time'] = starting['time'] + deltas['time']
+        try:
+            self.state['dive'] = deltas['dive']
+        try:
+            self.state['vel'] = deltas['vel']
+        # self.check_for_death()
 
-		# self.check_for_death()
+        print(f"At time {self.state['time']:.1f}: {self.state['alt']:.0f}ft,"
+              " Rng: {(self.state['rng']/6036):.1f}nm,"
+              " Dive: {self.state['dive']:.1f}deg")
 
-		print(f"At time {self.state['time']:.1f}: {self.state['alt']:.0f}ft, Rng: {(self.state['rng']/6036):.1f}nm, Dive: {self.state['dive']:.1f}deg")
+    # def check_for_death(self):
+    #   if self.state['time'] > self.missile.tof:
+    #       pass
+    #   else:
+    #       pass
 
-	# def check_for_death(self):
-	# 	if self.state['time'] > self.missile.tof:
-	# 		pass
-	# 	else:
-	# 		pass
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -7,17 +7,21 @@ class Calculator:
     def __init__(self, g_onset):
         self.g_onset = g_onset
 
-    def level_turn_radius(self, velocity, bank):  # bank in degrees, velocity in knots
-        return velocity**2 / (11.26 * tan(radians(bank))) # returns radius in feet
+    # get radius in feet
+    def level_turn_radius(self, velocity, bank):
+        return velocity**2 / (11.26 * tan(radians(bank)))
 
+    # get degrees per second
     def level_turn_rate(self, velocity, bank):
-        return (1091 * tan(radians(bank)))/velocity  # returns degrees per second
-     
-    def level_turn_g(self, bank):
-        return 1 / cos(radians(bank))  # returns g loading
+        return (1091 * tan(radians(bank)))/velocity
 
+    # get g loading for level turn
+    def level_turn_g(self, bank):
+        return 1 / cos(radians(bank))
+
+    # get bank based on g (level turn)
     def bank_for_g(self, g):
-        return degrees(acos(1/g))  # returns degrees of bank in level turn at "g" loading
+        return degrees(acos(1/g))
 
     def g_performance(self, vel, g):
         bank = self.bank_for_g(g)
@@ -30,39 +34,43 @@ class Calculator:
         rng = -vel*1.8781*time
         return {'time': time, 'alt': 0, 'rng': rng}
 
-    def first_90(self, vel, g, dive=0):  # returns (time, alt delta, range delta)
-        time = (90 / self.g_performance(vel, g)[0]) + (g/self.g_onset) # over simplified, need to integrate over time if want better acc.
+    def first_90(self, vel, g, dive=0):
+        # need better model for g onset
+        time = (90 / self.g_performance(vel, g)[0]) + (g/self.g_onset)
         alt = 0
-        rng = -self.g_performance(vel, g)[1]  # no penalty for g onset...need calculus // range is negative, ie. closure
+        rng = -self.g_performance(vel, g)[1]  # no penalty for g onset...
         return {'time': time, 'alt': alt, 'rng': rng}
-        
-    def second_90(self, vel, g, dive):  # returns (time, alt delta, range delta)
+
+    def second_90(self, vel, g, dive):
         time = 90 / self.g_performance(vel, g)[0]
         alt = sin(radians(dive))*self.g_performance(vel, g)[1]
-        rng = cos(radians(dive))*self.g_performance(vel, g)[1]  # range is increasing now
-        return {'time': time, 'alt': alt, 'rng': rng, 'dive':dive}
+        rng = cos(radians(dive))*self.g_performance(vel, g)[1]
+        return {'time': time, 'alt': alt, 'rng': rng, 'dive': dive}
 
-    def accel_in_dive(self, start_vel, end_vel, dive):  # should we be using mach or TAS?
-        accel_rate = 50  # const accel rate of 50 knots per second
+    def accel_in_dive(self, start_vel, end_vel, dive):
+        # const accel rate of 50 knots per second
+        accel_rate = 50
         time = (end_vel - start_vel) / accel_rate
         sin_dive = sin(radians(dive))
         cos_dive = cos(radians(dive))
         # get vertical descent rates in feet per second
-        st_vert_rate = sin_dive*start_vel*1.68781  # converts knots to fps
+        st_vert_rate = sin_dive*start_vel*1.68781  # knots to fps
         end_vert_rate = sin_dive*end_vel*1.68781
         # get horizontal travel rates in fps
         st_horiz_rate = cos_dive*start_vel*1.68781
         end_horiz_rate = cos_dive*end_vel*1.68781
         # calculate altitude lost during acceleration
-        alt = (st_vert_rate * time) + ((end_vert_rate * time)-(st_vert_rate*time))/2
+        alt = (st_vert_rate * time) + \
+              ((end_vert_rate * time)-(st_vert_rate*time))/2
         # calculate range delta during accel
-        rng = (st_horiz_rate * time) + ((end_horiz_rate * time)-(st_horiz_rate*time))/2
+        rng = (st_horiz_rate * time) + \
+              ((end_horiz_rate * time)-(st_horiz_rate*time))/2
         return {'time': time, 'alt': alt, 'rng': rng, 'vel': end_vel}
 
-    def straight_dive(self, vel, dive, st_alt):  # terminates at 4500' <-- hard coded for now
+    def straight_dive(self, vel, dive, st_alt):
         descent_rate = sin(radians(dive))*vel*1.68781
-        alt_to_lose = st_alt - (dive/0.01)
-        time = alt_to_lose / descent_rate  # time in seconds until at 1% of dive
+        alt_to_lose = st_alt - (dive/0.01)  # 1% dive angle
+        time = alt_to_lose / descent_rate  # time in seconds until recov.
         rng = cos(radians(dive))*vel*1.68781*time
         return {'time': time, 'alt': alt_to_lose, 'rng': rng}
 
@@ -72,17 +80,12 @@ class Calculator:
         dive = st_dive
         time = 0
         rng = 0
-        des = lambda x : sin(radians(x))*vel*1.68781  # feet per second
-        rng_inc = lambda x : cos(radians(x))*vel*1.68781
-        while  alt > min_alt:
+        def des(x): return sin(radians(x))*vel*1.68781  # feet per second
+        def rng_inc(x): return cos(radians(x))*vel*1.68781
+        while alt > min_alt:
             dive = alt * 0.01
             alt -= des(dive)
             rng += rng_inc(dive)
             time += 1
         dive = 0
-        return {'time': time, 'alt': st_alt-alt, 'rng': rng, 'dive' : dive}
-
-
-
-
-
+        return {'time': time, 'alt': st_alt - alt, 'rng': rng, 'dive': dive}
